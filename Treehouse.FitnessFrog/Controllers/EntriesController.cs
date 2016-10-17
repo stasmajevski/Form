389acc214b9e0265fcbe735d1a2d522dba55126c
User <User@DESKTOP-32B0IC3>
2016-10-17 18:14:03 +0300

    added post action method

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -41,9 +41,16 @@ namespace Treehouse.FitnessFrog.Controllers
 
         public ActionResult Add()
         {
+           
             return View();
         }
-
+        [ActionName("Add"),HttpPost]
+        
+        public ActionResult AddPost()
+        {
+            
+            return View();
+        }
         public ActionResult Edit(int? id)
         {
             if (id == null)
